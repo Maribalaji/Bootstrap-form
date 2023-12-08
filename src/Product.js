5a52b3387225c2379b4a6e25fa442abbe17cd1b4
Maribalaji <maribalaji008@gmail.com>
2023-12-08 10:58:40 +0530

    add

--- src/Product.js
+++ src/Product.js
@@ -8,7 +8,7 @@ function Product(){
     const {name,price,quantity,description}=useContext(context)
     return(
         <div>
-            <Card className="m-auto mt-5 bg-Info text-dark" border="dark" style={{ width: '18rem'}}>
+            <Card className="m-auto mt-5 bg-info text-dark" border="dark" style={{ width: '18rem'}}>
       <Card.Img variant="top" src="image/apple 14.jpg" />
       <Card.Body>
         <Card.Title>Product Details</Card.Title>
